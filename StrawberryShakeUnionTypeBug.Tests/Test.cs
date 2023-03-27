a549373ef5efb4bbc8601cc2c5304b8349e50d05
Sven Lübbehusen <s.luebbehusen@slashwhy.de>
2023-03-27 07:30:17 +0200

    add simple example

---
+++ StrawberryShakeUnionTypeBug.Tests/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExampleClient;
+using FluentAssertions;
+
+namespace StrawberryShakeUnionTypeBug.Tests;
+
+public class Test
+{
+    [Test]
+    public void GeneratedTypesShouldNotBeEmpty()
+    {
+        typeof(ExampleClient.ExampleQuery_ExampleQuery_TypeA).GetProperty("fieldA").Should().NotBeNull();
+        typeof(ExampleClient.ExampleQuery_ExampleQuery_TypeB).GetProperty("fieldB").Should().NotBeNull();
+    }
+}
